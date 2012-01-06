1ccf1d9ecd2e98d56e14ae18d9017f0b3a6b8556
Alessandro Rubini <rubini@gnudd.com>
2012-01-06 23:10:43 +0100

    bare-linux/bare-io.c: added missing lib functions
    Maybe we should consider some generic lib, as most of this can be
    shared across freestanding archs (we must check whether other
    freestanding places are really so lib-less os they have these minimal
    functions for example from newlib)
    
    Signed-off-by: Alessandro Rubini <rubini@gnudd.com>

--- arch-bare-linux/bare-io.c
+++ arch-bare-linux/bare-io.c
@@ -1,5 +1,5 @@
 /*
- * Alessandro Rubini for CERN, 2011 -- public domain
+ * Alessandro Rubini for CERN, 2011 -- GPL 2 or later (it includes u-boot code)
  */
 #include <pptp/pptp.h>
 #include "bare-linux.h"
@@ -33,6 +33,29 @@ void pp_get_stamp(uint32_t *sptr)
 	*sptr = htonl(sys_time(0));
 }
 
+int pp_memcmp(const void *cs, const void *ct, int count)
+{
+	/* from u-boot-1.1.2 */
+	const unsigned char *su1, *su2;
+	int res = 0;
+
+	for( su1 = cs, su2 = ct; 0 < count; ++su1, ++su2, count--)
+		if ((res = *su1 - *su2) != 0)
+			break;
+	return res;
+}
+
+void *pp_memset(void *s, int c, int count)
+{
+	/* from u-boot-1.1.2 */
+	char *xs = (char *) s;
+
+	while (count--)
+		*xs++ = c;
+
+	return s;
+}
+
 /* What follows has no prefix because it's only used by arch code */
 char *strcpy(char *dest, const char *src)
 {
@@ -45,23 +68,7 @@ char *strcpy(char *dest, const char *src)
 }
 
 void *memset(void *s, int c, int count)
-{
-	/* from u-boot-1.1.2 */
-	char *xs = (char *) s;
-
-	while (count--)
-		*xs++ = c;
-
-	return s;
-}
+	__attribute__((alias("pp_memset")));
 
 void *memcpy(void *dest, const void *src, int count)
-{
-	/* from u-boot-1.1.2 */
-	char *tmp = (char *) dest, *s = (char *) src;
-
-	while (count--)
-		*tmp++ = *s++;
-
-	return dest;
-}
+	__attribute__((alias("pp_memcpy")));
