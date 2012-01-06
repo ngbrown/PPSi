205d324eed65ca717e2ff8c798cbd18828c7d8d2
Alessandro Rubini <rubini@gnudd.com>
2012-01-06 23:33:37 +0100

    lib/div64.c: new file
    This is the 64-bit unsigned division and remainder from the Linux
    kernel, for 32-bit host systems.
    
    The new lib/ directory can be used by archs to pick files, and that's
    already exemplified by arch-gnu-linux and arch-bare-linux.
    
    Signed-off-by: Alessandro Rubini <rubini@gnudd.com>

--- include/pptp/lib.h
+++ include/pptp/lib.h
@@ -3,6 +3,7 @@
  */
 #ifndef __PTP_LIB_H__
 #define __PTP_LIB_H__
+#include <stdint.h>
 
 /* We base on puts and a few more functions: each arch must have it */
 extern void pp_puts(const char *s);
@@ -10,5 +11,7 @@ extern int pp_strnlen(const char *s, int maxlen);
 extern void *pp_memcpy(void *d, const void *s, int count);
 extern int pp_memcmp(const void *s1, const void *s2, int count);
 extern void *pp_memset(void *s, int c, int count);
+
+extern uint32_t __div64_32(uint64_t *n, uint32_t base);
 
 #endif /* __PTP_DEP_H__ */
