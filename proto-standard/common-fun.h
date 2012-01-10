f1faad4fa3398174246e7838f94b3587f6eb9fd1
Aurelio Colosimo <aurelio@aureliocolosimo.it>
2012-01-10 09:39:57 +0100

    trivial: deleted commented out code
    previously commented "st_com_add_foreign" header is now removed (not used
    externally of common-fun.c)

--- proto-standard/common-fun.h
+++ proto-standard/common-fun.h
@@ -12,10 +12,6 @@ void st_com_restart_annrec_timer(struct pp_instance *ppi);
 
 int st_com_check_record_update(struct pp_instance *ppi);
 
-/*
-void st_com_add_foreign(unsigned char *buf, MsgHeader *header,
-			struct pp_instance *ppi);
-*/
 
 int st_com_slave_handle_announce(struct pp_instance *ppi, unsigned char *buf,
 				 int len);
