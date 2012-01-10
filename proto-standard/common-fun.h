ae2896328be9fb179c970898a019b57e3fcc4156
Aurelio Colosimo <aurelio@aureliocolosimo.it>
2012-01-10 11:24:38 +0100

    trivial: added a comment about *_handle_* functions return value

--- proto-standard/common-fun.h
+++ proto-standard/common-fun.h
@@ -13,7 +13,10 @@ void st_com_restart_annrec_timer(struct pp_instance *ppi);
 
 int st_com_check_record_update(struct pp_instance *ppi);
 
-
+/* Each of the following "handle" functions" return 0 in case of correct
+ * message, -1 in case the message contained in buf is not proper (e.g. size
+ * is not the expected one
+ */
 int st_com_slave_handle_announce(struct pp_instance *ppi, unsigned char *buf,
 				 int len);
 
