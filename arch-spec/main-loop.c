c2dbe3c694c83207ba5d83f06ebe3f616f19d849
Grzegorz Daniluk <g.daniluk@elproma.com.pl>
2012-03-28 02:29:07 +0200

    spec: remove unnecessary printfs

--- arch-spec/main-loop.c
+++ arch-spec/main-loop.c
@@ -23,13 +23,11 @@ void spec_main_loop(struct pp_instance *ppi)
 	while (1) {
 		unsigned char packet[1500];
 		/* Wait for a packet or for the timeout */
-		pp_printf("wait for packet\n");
 		while (delay_ms && !minic_poll_rx()) {
 			spec_udelay(1000);
 			delay_ms--;
 		}
 		if (!minic_poll_rx()) {
-			pp_printf("timeout\n");
 			delay_ms = pp_state_machine(ppi, NULL, 0);
 			continue;
 		}
