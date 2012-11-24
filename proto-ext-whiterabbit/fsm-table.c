f7f11d52571a20a23132d400187f00c4aa950a08
Aurelio Colosimo <aurelio@aureliocolosimo.it>
2012-11-24 16:25:10 +0100

    proto-ext-whiterabbit: do not compile pre-master and passive state
    Pre-Master and Passive state are not used in the current implementation
    of WR protocol, so do not compile nor link them, in order to save space
    
    Signed-off-by: Aurelio Colosimo <aurelio@aureliocolosimo.it>

--- proto-ext-whiterabbit/fsm-table.c
+++ proto-ext-whiterabbit/fsm-table.c
@@ -16,9 +16,9 @@ struct pp_state_table_item pp_state_table[] __weak = {
 	{ PPS_FAULTY,		"faulty",	pp_faulty,},
 	{ PPS_DISABLED,		"disabled",	pp_disabled,},
 	{ PPS_LISTENING,	"listening",	pp_listening,},
-	{ PPS_PRE_MASTER,	"pre-master",	pp_pre_master,},
+	/*{ PPS_PRE_MASTER,	"pre-master",	pp_pre_master,},*/
 	{ PPS_MASTER,		"master",	pp_master,},
-	{ PPS_PASSIVE,		"passive",	pp_passive,},
+	/*{ PPS_PASSIVE,	"passive",	pp_passive,},*/
 	{ PPS_UNCALIBRATED,	"uncalibrated",	pp_uncalibrated,},
 	{ PPS_SLAVE,		"slave",	pp_slave,},
 	{ WRS_PRESENT,		"uncalibrated/wr-present",	wr_present,},
