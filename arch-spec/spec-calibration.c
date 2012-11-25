1eb66f6704647cca1ce2fd9980077a352e9ac82b
Aurelio Colosimo <aurelio@aureliocolosimo.it>
2012-11-25 12:11:09 +0100

    arch-spec/spec-calibration.c use spec_read_calibration_data
    spec_calibrating_poll was using ep_get_deltas instead of
    spec_read_calibration_data, like freestanding-wrapper.c does in wrpc-sw.
    The whole spec-calibration.c is obsolete, since it should simply be a
    wrapper for wrpc-sw functions defined in
    ptp-noposix/libposix/freestanding-wrapper.c
    
    Signed-off-by: Aurelio Colosimo <aurelio@aureliocolosimo.it>

--- arch-spec/spec-calibration.c
+++ arch-spec/spec-calibration.c
@@ -66,8 +66,9 @@ int spec_calibrating_poll(struct pp_instance *ppi, int txrx, uint32_t *delta)
 {
 	uint32_t delta_rx = 0, delta_tx = 0;
 
-	/* FIXME: why delta is 64bit whereas ep_get_deltas accepts 32bit? */
-	ep_get_deltas( &delta_tx, &delta_rx);
+	/* FIXME: why delta was 64bit whereas ep_get_deltas accepts 32bit? */
+	spec_read_calibration_data(OPTS(ppi)->iface_name,
+		&delta_tx, &delta_rx, NULL, NULL);
 
 	if (txrx == WR_HW_CALIB_TX)
 		*delta = delta_tx;
