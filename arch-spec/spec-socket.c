d374d174c71f23acfea61674e9b9fd2aaadc8f74
Aurelio Colosimo <aurelio@aureliocolosimo.it>
2013-01-07 14:23:10 +0100

    trivial: clean log message in spec_net_init
    Signed-off-by: Aurelio Colosimo <aurelio@aureliocolosimo.it>

--- arch-spec/spec-socket.c
+++ arch-spec/spec-socket.c
@@ -106,9 +106,8 @@ int spec_net_init(struct pp_instance *ppi)
 	ppi->buf_out = buffer_out;
 	ppi->buf_out = PROTO_PAYLOAD(ppi->buf_out);
 
-	ppi->buf_out+= 4 - (((int)ppi->buf_out) % 4);
-	/* FIXME Alignment */
-	pp_printf("spec_net_init ETH %x\n", (uint32_t)ppi->buf_out);
+	ppi->buf_out+= 4 - (((int)ppi->buf_out) % 4); /* FIXME Alignment */
+	pp_printf("spec_net_init\n");
 	spec_open_ch(ppi);
 
 	return 0;
