afd96c2e50373a771730f6375c7589f6aa9a7635
Danilo Sabato <force84@gmail.com>
2013-01-26 22:16:42 +0100

    arch-bare-linux: added set/get_tstamp and adj_freq using syscall

--- arch-bare-linux/bare-io.c
+++ arch-bare-linux/bare-io.c
@@ -2,7 +2,10 @@
  * Alessandro Rubini for CERN, 2011 -- GPL 2 or later (it includes u-boot code)
  */
 #include <ppsi/ppsi.h>
+#include <ppsi/diag.h>
 #include "bare-linux.h"
+
+const Integer32 PP_ADJ_FREQ_MAX = 512000;
 
 void pp_puts(const char *s)
 {
@@ -28,9 +31,38 @@ void *pp_memcpy(void *dest, const void *src, int count)
 	return dest;
 }
 
-void pp_get_tstamp(TimeInternal *t)
+void bare_get_tstamp(TimeInternal *t)
 {
-	/* FIXME tstamp *sptr = htonl(sys_time(0));*/
+	struct bare_timeval tv;
+
+	if (sys_gettimeofday(&tv, NULL) < 0) {
+		PP_PRINTF("gettimeofday error");
+		sys_exit(0);
+	}
+
+	t->seconds = tv.tv_sec;
+	t->nanoseconds = (tv.tv_usec % 1000) * 1000;
+}
+
+int32_t bare_set_tstamp(TimeInternal *t)
+{
+	struct bare_timeval tv_orig;
+	struct bare_timeval tv;
+
+	if (sys_gettimeofday(&tv_orig, NULL) < 0) {
+		PP_PRINTF("gettimeofday error");
+		sys_exit(0);
+	}
+
+	tv.tv_sec = t->seconds;
+	tv.tv_usec = t->nanoseconds / 1000;
+
+	if (sys_settimeofday(&tv, NULL) < 0) {
+		PP_PRINTF("settimeofday error");
+		sys_exit(0);
+	}
+
+	return tv.tv_sec - tv_orig.tv_sec;
 }
 
 int pp_memcmp(const void *cs, const void *ct, int count)
@@ -69,6 +101,28 @@ char *strcpy(char *dest, const char *src)
 
 void *memset(void *s, int c, int count)
 	__attribute__((alias("pp_memset")));
-
 void *memcpy(void *dest, const void *src, int count)
 	__attribute__((alias("pp_memcpy")));
+
+int bare_adj_freq(Integer32 adj)
+{
+	struct bare_timex t;
+
+        if (adj > PP_ADJ_FREQ_MAX)
+                adj = PP_ADJ_FREQ_MAX;
+        else if (adj < -PP_ADJ_FREQ_MAX)
+                adj = -PP_ADJ_FREQ_MAX;
+
+        t.modes = MOD_FREQUENCY;
+        t.freq = adj * ((1 << 16) / 1000);
+
+        return !sys_adjtimex(&t);
+}
+
+int32_t pp_set_tstamp(TimeInternal *t)
+        __attribute__((alias("bare_set_tstamp")));
+void pp_get_tstamp(TimeInternal *t)
+        __attribute__((alias("bare_get_tstamp")));
+int pp_adj_freq(Integer32 adj)
+        __attribute__((alias("bare_adj_freq")));
+
