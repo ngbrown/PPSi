158c7cd95f0e21f7ea40a39ba09d6c05a151e603
Danilo Sabato <force84@gmail.com>
2013-01-30 18:26:59 +0100

    arch-bare-x86-64: #define sys clocks copied from linux/time.h

--- arch-bare-x86-64/bare-linux.h
+++ arch-bare-x86-64/bare-linux.h
@@ -144,3 +144,17 @@ struct bare_timex {
   int  :32; int  :32; int  :32; int  :32;
   int  :32; int  :32; int  :32;
 };
+
+
+/*
+ * Copy from <linux/time.h>
+ *
+ * The IDs of the various system clocks (for POSIX.1b interval timers):
+ */
+#define CLOCK_REALTIME                  0
+#define CLOCK_MONOTONIC                 1
+#define CLOCK_PROCESS_CPUTIME_ID        2
+#define CLOCK_THREAD_CPUTIME_ID         3
+#define CLOCK_MONOTONIC_RAW             4
+#define CLOCK_REALTIME_COARSE           5
+#define CLOCK_MONOTONIC_COARSE          6
