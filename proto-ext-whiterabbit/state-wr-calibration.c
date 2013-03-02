ngbrown/PPSi

commit 44deab24fbd52593147b5b5385a6e58ba7eff135

proto-ext-whiterabbit: split out wr-specific portDS
struct DSPort now has a void pointer called ext_dsport that is used by
each extension to store its own local data.  White Rabbit, thus,
is not a special case any more in this respect.

Signed-off-by: Alessandro Rubini <rubini@gnudd.com>

proto-ext-whiterabbit/state-wr-calibration.c
@@ -13,31 +13,31 @@ int wr_calibration(struct pp_instance *ppi, unsigned char *pkt, int plen)
 	uint32_t delta;
 
 	if (ppi->is_new_state) {
-		DSPOR(ppi)->wrPortState = WRS_CALIBRATION;
+		WR_DSPOR(ppi)->wrPortState = WRS_CALIBRATION;
 
 		e = msg_issue_wrsig(ppi, CALIBRATE);
-		pp_timer_start(DSPOR(ppi)->calPeriod,
+		pp_timer_start(WR_DSPOR(ppi)->calPeriod,
 			ppi->timers[PP_TIMER_WRS_CALIBRATION]);
-		if (DSPOR(ppi)->calibrated) {
-			DSPOR(ppi)->wrPortState = WRS_CALIBRATION_2;
+		if (WR_DSPOR(ppi)->calibrated) {
+			WR_DSPOR(ppi)->wrPortState = WRS_CALIBRATION_2;
 		}
 	}
 
 	if (pp_timer_expired(ppi->timers[PP_TIMER_WRS_CALIBRATION])) {
-		if (DSPOR(ppi)->wrMode == WR_MASTER)
+		if (WR_DSPOR(ppi)->wrMode == WR_MASTER)
 			ppi->next_state = PPS_MASTER;
 		else
 			ppi->next_state = PPS_LISTENING;
-		DSPOR(ppi)->wrPortState = WRS_IDLE;
+		WR_DSPOR(ppi)->wrPortState = WRS_IDLE;
 		goto state_updated;
 	}
 
-	switch (DSPOR(ppi)->wrPortState) {
+	switch (WR_DSPOR(ppi)->wrPortState) {
 	case WRS_CALIBRATION:
 		/* enable pattern sending */
 		if (wr_calibration_pattern_enable(ppi, 0, 0, 0) ==
 			WR_HW_CALIB_OK)
-			DSPOR(ppi)->wrPortState = WRS_CALIBRATION_1;
+			WR_DSPOR(ppi)->wrPortState = WRS_CALIBRATION_1;
 		else
 			break;
 
@@ -45,7 +45,7 @@ int wr_calibration(struct pp_instance *ppi, unsigned char *pkt, int plen)
 		/* enable Tx calibration */
 		if (wr_calibrating_enable(ppi, WR_HW_CALIB_TX)
 				== WR_HW_CALIB_OK)
-			DSPOR(ppi)->wrPortState = WRS_CALIBRATION_2;
+			WR_DSPOR(ppi)->wrPortState = WRS_CALIBRATION_2;
 		else
 			break;
 
@@ -53,16 +53,16 @@ int wr_calibration(struct pp_instance *ppi, unsigned char *pkt, int plen)
 		/* wait until Tx calibration is finished */
 		if (wr_calibrating_poll(ppi, WR_HW_CALIB_TX, &delta) ==
 			WR_HW_CALIB_READY) {
-			DSPOR(ppi)->deltaTx.scaledPicoseconds.msb =
+			WR_DSPOR(ppi)->deltaTx.scaledPicoseconds.msb =
 				0xFFFFFFFF & (((uint64_t)delta) >> 16);
-			DSPOR(ppi)->deltaTx.scaledPicoseconds.lsb =
+			WR_DSPOR(ppi)->deltaTx.scaledPicoseconds.lsb =
 				0xFFFFFFFF & (((uint64_t)delta) << 16);
 			PP_PRINTF("Tx=>>scaledPicoseconds.msb = 0x%x\n",
 				DSPOR(ppi)->deltaTx.scaledPicoseconds.msb);
 			PP_PRINTF("Tx=>>scaledPicoseconds.lsb = 0x%x\n",
 				DSPOR(ppi)->deltaTx.scaledPicoseconds.lsb);
 
-			DSPOR(ppi)->wrPortState = WRS_CALIBRATION_3;
+			WR_DSPOR(ppi)->wrPortState = WRS_CALIBRATION_3;
 		}
 		else
 			break; /* again */
@@ -71,14 +71,14 @@ int wr_calibration(struct pp_instance *ppi, unsigned char *pkt, int plen)
 		/* disable Tx calibration */
 		if (wr_calibrating_disable(ppi, WR_HW_CALIB_TX)
 				== WR_HW_CALIB_OK)
-			DSPOR(ppi)->wrPortState = WRS_CALIBRATION_4;
+			WR_DSPOR(ppi)->wrPortState = WRS_CALIBRATION_4;
 		else
 			break;
 
 	case WRS_CALIBRATION_4:
 		/* disable pattern sending */
 		if (wr_calibration_pattern_disable(ppi) == WR_HW_CALIB_OK)
-			DSPOR(ppi)->wrPortState = WRS_CALIBRATION_5;
+			WR_DSPOR(ppi)->wrPortState = WRS_CALIBRATION_5;
 		else
 			break;
 
@@ -86,7 +86,7 @@ int wr_calibration(struct pp_instance *ppi, unsigned char *pkt, int plen)
 		/* enable Rx calibration using the pattern sent by other port */
 		if (wr_calibrating_enable(ppi, WR_HW_CALIB_RX) ==
 				WR_HW_CALIB_OK)
-			DSPOR(ppi)->wrPortState = WRS_CALIBRATION_6;
+			WR_DSPOR(ppi)->wrPortState = WRS_CALIBRATION_6;
 		else
 			break;
 
@@ -95,16 +95,16 @@ int wr_calibration(struct pp_instance *ppi, unsigned char *pkt, int plen)
 		if (wr_calibrating_poll(ppi, WR_HW_CALIB_RX, &delta) ==
 			WR_HW_CALIB_READY) {
 			PP_PRINTF("Rx fixed delay = %d\n", (int)delta);
-			DSPOR(ppi)->deltaRx.scaledPicoseconds.msb =
+			WR_DSPOR(ppi)->deltaRx.scaledPicoseconds.msb =
 				0xFFFFFFFF & (delta >> 16);
-			DSPOR(ppi)->deltaRx.scaledPicoseconds.lsb =
+			WR_DSPOR(ppi)->deltaRx.scaledPicoseconds.lsb =
 				0xFFFFFFFF & (delta << 16);
 			PP_PRINTF("Rx=>>scaledPicoseconds.msb = 0x%x\n",
 				DSPOR(ppi)->deltaRx.scaledPicoseconds.msb);
 			PP_PRINTF("Rx=>>scaledPicoseconds.lsb = 0x%x\n",
 				DSPOR(ppi)->deltaRx.scaledPicoseconds.lsb);
 
-			DSPOR(ppi)->wrPortState = WRS_CALIBRATION_7;
+			WR_DSPOR(ppi)->wrPortState = WRS_CALIBRATION_7;
 		}
 		else
 			break; /* again */
@@ -113,14 +113,14 @@ int wr_calibration(struct pp_instance *ppi, unsigned char *pkt, int plen)
 		/* disable Rx calibration */
 		if (wr_calibrating_disable(ppi, WR_HW_CALIB_RX)
 				== WR_HW_CALIB_OK)
-			DSPOR(ppi)->wrPortState = WRS_CALIBRATION_8;
+			WR_DSPOR(ppi)->wrPortState = WRS_CALIBRATION_8;
 		else
 			break;
 	case WRS_CALIBRATION_8:
 		/* send deltas to the other port and go to the next state */
 		e = msg_issue_wrsig(ppi, CALIBRATED);
 		ppi->next_state = WRS_CALIBRATED;
-		DSPOR(ppi)->calibrated = TRUE;
+		WR_DSPOR(ppi)->calibrated = TRUE;
 
 	default:
 		break;
@@ -130,7 +130,7 @@ state_updated:
 	if (ppi->next_state != ppi->state)
 		pp_timer_stop(ppi->timers[PP_TIMER_WRS_CALIBRATION]);
 
-	ppi->next_delay = DSPOR(ppi)->wrStateTimeout;
+	ppi->next_delay = WR_DSPOR(ppi)->wrStateTimeout;
 
 	return e;
 }

proto-ext-whiterabbit/wr-msg.c
@@ -69,12 +69,12 @@ void msg_pack_announce_wr_tlv(struct pp_instance *ppi)
 		| WR_TLV_WR_VERSION_NUMBER)));
 	//wrMessageId
 	*(UInteger16*)(buf + 74) = htons(ANN_SUFIX);
-	wr_flags = wr_flags | DSPOR(ppi)->wrConfig;
+	wr_flags = wr_flags | WR_DSPOR(ppi)->wrConfig;
 
-	if (DSPOR(ppi)->calibrated)
+	if (WR_DSPOR(ppi)->calibrated)
 		wr_flags = WR_IS_CALIBRATED | wr_flags;
 
-	if (DSPOR(ppi)->wrModeOn)
+	if (WR_DSPOR(ppi)->wrModeOn)
 		wr_flags = WR_IS_WR_MODE | wr_flags;
 	*(UInteger16*)(buf + 76) = htons(wr_flags);
 }
@@ -112,7 +112,7 @@ int msg_pack_wrsig(struct pp_instance *ppi, Enumeration16 wr_msg_id)
 	void *buf;
 	UInteger16 len = 0;
 
-	if ((DSPOR(ppi)->wrMode == NON_WR) || (wr_msg_id == ANN_SUFIX)) {
+	if ((WR_DSPOR(ppi)->wrMode == NON_WR) || (wr_msg_id == ANN_SUFIX)) {
 		PP_PRINTF("BUG: Trying to send invalid wr_msg mode=%x id=%x",
 			  DSPOR(ppi)->wrMode, wr_msg_id);
 		return 0;
@@ -144,24 +144,26 @@ int msg_pack_wrsig(struct pp_instance *ppi, Enumeration16 wr_msg_id)
 
 	switch (wr_msg_id) {
 	case CALIBRATE:
-		if(DSPOR(ppi)->calibrated) {
-			put_be16(buf+56, (DSPOR(ppi)->calRetry << 8 | 0x0000));
+		if(WR_DSPOR(ppi)->calibrated) {
+			put_be16(buf+56,
+				 (WR_DSPOR(ppi)->calRetry << 8 | 0x0000));
 		}
 		else {
-			put_be16(buf+56, (DSPOR(ppi)->calRetry << 8 | 0x0001));
+			put_be16(buf+56,
+				 (WR_DSPOR(ppi)->calRetry << 8 | 0x0001));
 		}
-		put_be32(buf+58, DSPOR(ppi)->calPeriod);
+		put_be32(buf+58, WR_DSPOR(ppi)->calPeriod);
 		len = 20;
 		break;
 
 	case CALIBRATED: /* new fsm */
 		/* delta TX */
-		put_be32(buf+56, DSPOR(ppi)->deltaTx.scaledPicoseconds.msb);
-		put_be32(buf+60, DSPOR(ppi)->deltaTx.scaledPicoseconds.lsb);
+		put_be32(buf+56, WR_DSPOR(ppi)->deltaTx.scaledPicoseconds.msb);
+		put_be32(buf+60, WR_DSPOR(ppi)->deltaTx.scaledPicoseconds.lsb);
 
 		/* delta RX */
-		put_be32(buf+64, DSPOR(ppi)->deltaRx.scaledPicoseconds.msb);
-		put_be32(buf+68, DSPOR(ppi)->deltaRx.scaledPicoseconds.lsb);
+		put_be32(buf+64, WR_DSPOR(ppi)->deltaRx.scaledPicoseconds.msb);
+		put_be32(buf+68, WR_DSPOR(ppi)->deltaRx.scaledPicoseconds.lsb);
 		len = 24;
 
 		break;
@@ -235,22 +237,24 @@ void msg_unpack_wrsig(struct pp_instance *ppi, void *buf,
 
 	switch (wr_msg_id) {
 	case CALIBRATE:
-		DSPOR(ppi)->otherNodeCalSendPattern = 0x00FF & get_be16(buf+56);
-		DSPOR(ppi)->otherNodeCalRetry = 0x00FF & (get_be16(buf+56) >> 8);
-		DSPOR(ppi)->otherNodeCalPeriod = get_be32(buf+58);
+		WR_DSPOR(ppi)->otherNodeCalSendPattern =
+			0x00FF & get_be16(buf+56);
+		WR_DSPOR(ppi)->otherNodeCalRetry =
+			0x00FF & (get_be16(buf+56) >> 8);
+		WR_DSPOR(ppi)->otherNodeCalPeriod = get_be32(buf+58);
 		break;
 
 	case CALIBRATED:
 		/* delta TX */
-		DSPOR(ppi)->otherNodeDeltaTx.scaledPicoseconds.msb =
+		WR_DSPOR(ppi)->otherNodeDeltaTx.scaledPicoseconds.msb =
 			get_be32(buf+56);
-		DSPOR(ppi)->otherNodeDeltaTx.scaledPicoseconds.lsb =
+		WR_DSPOR(ppi)->otherNodeDeltaTx.scaledPicoseconds.lsb =
 			get_be32(buf+60);
 
 		/* delta RX */
-		DSPOR(ppi)->otherNodeDeltaRx.scaledPicoseconds.msb =
+		WR_DSPOR(ppi)->otherNodeDeltaRx.scaledPicoseconds.msb =
 			get_be32(buf+64);
-		DSPOR(ppi)->otherNodeDeltaRx.scaledPicoseconds.lsb =
+		WR_DSPOR(ppi)->otherNodeDeltaRx.scaledPicoseconds.lsb =
 			get_be32(buf+68);
 		break;
 

include/ppsi/ieee1588_types.h
@@ -254,35 +254,7 @@ typedef struct DSPort {
 	Integer8	logMinPdelayReqInterval;
 	UInteger4	versionNumber;
 
-	/* White Rabbit extension begin (see wrspec.v2-06-07-2011, page 17) */
-	Enumeration8 wrConfig;
-	Enumeration8 wrMode;
-	Boolean wrModeOn;
-	Enumeration8  wrPortState;
-	/* FIXME check doc: knownDeltaTx, knownDeltaRx, deltasKnown?) */
-	Boolean calibrated;
-	FixedDelta deltaTx;
-	FixedDelta deltaRx;
-	UInteger32 wrStateTimeout;
-	UInteger8 wrStateRetry;
-	UInteger32 calPeriod;
-	UInteger8 calRetry;
-	Enumeration8 parentWrConfig;
-	Boolean parentIsWRnode; /* FIXME Not in the doc */
-	/* FIXME check doc: (parentWrMode?) */
-	Enumeration16 msgTmpWrMessageID; /* FIXME Not in the doc */
-	Boolean parentWrModeOn;
-	Boolean parentCalibrated;
-
-	/* FIXME: are they in the doc? */
-	UInteger16 otherNodeCalSendPattern;
-	UInteger32 otherNodeCalPeriod;
-	UInteger8 otherNodeCalRetry;
-	FixedDelta otherNodeDeltaTx;
-	FixedDelta otherNodeDeltaRx;
-	Boolean doRestart;
-	Boolean linkUP;
-	/* White Rabbit extension end */
+	void		*ext_dsport;
 } DSPort;
 
 /* Time Properties Data Set */

proto-ext-whiterabbit/bmc.c
@@ -87,12 +87,13 @@ void s1(struct pp_instance *ppi, MsgHeader *hdr, MsgAnnounce *ann)
 	DSPRO(ppi)->timeSource = ann->timeSource;
 
 	/* White Rabbit */
-	DSPOR(ppi)->parentIsWRnode = ((ann->wrFlags & WR_NODE_MODE) != NON_WR);
-	DSPOR(ppi)->parentWrModeOn =
+	WR_DSPOR(ppi)->parentIsWRnode =
+		((ann->wrFlags & WR_NODE_MODE) != NON_WR);
+	WR_DSPOR(ppi)->parentWrModeOn =
 		(ann->wrFlags & WR_IS_WR_MODE) ? TRUE : FALSE;
-	DSPOR(ppi)->parentCalibrated =
+	WR_DSPOR(ppi)->parentCalibrated =
 			((ann->wrFlags & WR_IS_CALIBRATED) ? 1 : 0);
-	DSPOR(ppi)->parentWrConfig = ann->wrFlags & WR_NODE_MODE;
+	WR_DSPOR(ppi)->parentWrConfig = ann->wrFlags & WR_NODE_MODE;
 	DSCUR(ppi)->primarySlavePortNumber =
 		DSPOR(ppi)->portIdentity.portNumber;
 

proto-ext-whiterabbit/common-fun.c
@@ -12,10 +12,10 @@ int st_com_execute_slave(struct pp_instance *ppi, int check_delayreq)
 {
 	int ret = 0;
 
-	if (DSPOR(ppi)->doRestart) {
+	if (WR_DSPOR(ppi)->doRestart) {
 		ppi->next_state = PPS_INITIALIZING;
 		st_com_restart_annrec_timer(ppi);
-		DSPOR(ppi)->doRestart = FALSE;
+		WR_DSPOR(ppi)->doRestart = FALSE;
 		return 0;
 	}
 
@@ -178,10 +178,10 @@ int st_com_slave_handle_announce(struct pp_instance *ppi, unsigned char *buf,
 		st_com_add_foreign(ppi, buf);
 	}
 
-	if ((DSPOR(ppi)->wrConfig & WR_S_ONLY) &&
+	if ((WR_DSPOR(ppi)->wrConfig & WR_S_ONLY) &&
 	    (1 /* FIXME: Recommended State, see page 33*/) &&
-	    (DSPOR(ppi)->parentWrConfig & WR_M_ONLY) &&
-	    (!DSPOR(ppi)->wrModeOn || !DSPOR(ppi)->parentWrModeOn))
+	    (WR_DSPOR(ppi)->parentWrConfig & WR_M_ONLY) &&
+	    (!WR_DSPOR(ppi)->wrModeOn || !WR_DSPOR(ppi)->parentWrModeOn))
 		ppi->next_state = WRS_PRESENT;
 
 	/*Reset Timer handling Announce receipt timeout*/
@@ -286,7 +286,7 @@ int st_com_slave_handle_followup(struct pp_instance *ppi, unsigned char *buf,
 	add_TimeInternal(&correction_field, &correction_field,
 		&ppi->last_sync_corr_field);
 
-	if (!DSPOR(ppi)->wrModeOn) {
+	if (!WR_DSPOR(ppi)->wrModeOn) {
 		pp_update_offset(ppi, &precise_orig_timestamp,
 			&ppi->sync_receive_time,
 			&correction_field);

proto-ext-whiterabbit/msg.c
@@ -229,8 +229,8 @@ int msg_pack_announce(struct pp_instance *ppi)
 	*(UInteger16 *) (buf + 61) = htons(DSCUR(ppi)->stepsRemoved);
 	*(Enumeration8 *) (buf + 63) = DSPRO(ppi)->timeSource;
 
-	if (DSPOR(ppi)->wrConfig != NON_WR &&
-		DSPOR(ppi)->wrConfig != WR_S_ONLY) {
+	if (WR_DSPOR(ppi)->wrConfig != NON_WR &&
+		WR_DSPOR(ppi)->wrConfig != WR_S_ONLY) {
 		msg_pack_announce_wr_tlv(ppi);
 		return WR_ANNOUNCE_LENGTH;
 	}

proto-ext-whiterabbit/state-initializing.c
@@ -68,12 +68,12 @@ int pp_initializing(struct pp_instance *ppi, unsigned char *pkt, int plen)
 	DSPOR(ppi)->delayMechanism = PP_DEFAULT_DELAY_MECHANISM;
 	DSPOR(ppi)->logMinPdelayReqInterval = PP_DEFAULT_PDELAYREQ_INTERVAL;
 	DSPOR(ppi)->versionNumber = PP_VERSION_PTP;
-	DSPOR(ppi)->wrStateTimeout = WR_DEFAULT_STATE_TIMEOUT_MS;
-	DSPOR(ppi)->wrStateRetry = WR_DEFAULT_STATE_REPEAT;
-	DSPOR(ppi)->calPeriod = WR_DEFAULT_CAL_PERIOD;
-	DSPOR(ppi)->wrModeOn = 0;
-	DSPOR(ppi)->parentWrConfig = 0;
-	DSPOR(ppi)->calibrated = !WR_DEFAULT_PHY_CALIBRATION_REQUIRED;
+	WR_DSPOR(ppi)->wrStateTimeout = WR_DEFAULT_STATE_TIMEOUT_MS;
+	WR_DSPOR(ppi)->wrStateRetry = WR_DEFAULT_STATE_REPEAT;
+	WR_DSPOR(ppi)->calPeriod = WR_DEFAULT_CAL_PERIOD;
+	WR_DSPOR(ppi)->wrModeOn = 0;
+	WR_DSPOR(ppi)->parentWrConfig = 0;
+	WR_DSPOR(ppi)->calibrated = !WR_DEFAULT_PHY_CALIBRATION_REQUIRED;
 
 	if (pp_timer_init(ppi))
 		goto failure;

proto-ext-whiterabbit/state-listening.c
@@ -14,8 +14,8 @@ int pp_listening(struct pp_instance *ppi, unsigned char *pkt, int plen)
 
 	if (ppi->is_new_state) {
 		DSPOR(ppi)->portState = PPS_LISTENING;
-		DSPOR(ppi)->wrMode = NON_WR;
-		DSPOR(ppi)->wrPortState = WRS_IDLE;
+		WR_DSPOR(ppi)->wrMode = NON_WR;
+		WR_DSPOR(ppi)->wrPortState = WRS_IDLE;
 		st_com_restart_annrec_timer(ppi);
 	}
 

commit 462a3662bd7edd2df556413a66a7b787fe923473

bugfix: ext-whiterabbit: fix some diagnostics
This fixes a bug introduced in 44deab24 (97 commits ago), where I
split WR_DSPOR(ppi) out of DSPOR(ppi), but not all diagnostics has
ever been built. Next commit fixes this, and allowed me to make this
very fix.

Signed-off-by: Alessandro Rubini <rubini@gnudd.com>

proto-ext-whiterabbit/state-wr-calibration.c
@@ -57,9 +57,9 @@ int wr_calibration(struct pp_instance *ppi, unsigned char *pkt, int plen)
 			WR_DSPOR(ppi)->deltaTx.scaledPicoseconds.lsb =
 				0xFFFFFFFF & (((uint64_t)delta) << 16);
 			PP_PRINTF("Tx=>>scaledPicoseconds.msb = 0x%x\n",
-				DSPOR(ppi)->deltaTx.scaledPicoseconds.msb);
+				WR_DSPOR(ppi)->deltaTx.scaledPicoseconds.msb);
 			PP_PRINTF("Tx=>>scaledPicoseconds.lsb = 0x%x\n",
-				DSPOR(ppi)->deltaTx.scaledPicoseconds.lsb);
+				WR_DSPOR(ppi)->deltaTx.scaledPicoseconds.lsb);
 
 			WR_DSPOR(ppi)->wrPortState = WRS_CALIBRATION_3;
 		} else {
@@ -99,9 +99,9 @@ int wr_calibration(struct pp_instance *ppi, unsigned char *pkt, int plen)
 			WR_DSPOR(ppi)->deltaRx.scaledPicoseconds.lsb =
 				0xFFFFFFFF & (delta << 16);
 			PP_PRINTF("Rx=>>scaledPicoseconds.msb = 0x%x\n",
-				DSPOR(ppi)->deltaRx.scaledPicoseconds.msb);
+				WR_DSPOR(ppi)->deltaRx.scaledPicoseconds.msb);
 			PP_PRINTF("Rx=>>scaledPicoseconds.lsb = 0x%x\n",
-				DSPOR(ppi)->deltaRx.scaledPicoseconds.lsb);
+				WR_DSPOR(ppi)->deltaRx.scaledPicoseconds.lsb);
 
 			WR_DSPOR(ppi)->wrPortState = WRS_CALIBRATION_7;
 		} else {

proto-ext-whiterabbit/wr-msg.c
@@ -114,7 +114,7 @@ int msg_pack_wrsig(struct pp_instance *ppi, Enumeration16 wr_msg_id)
 
 	if ((WR_DSPOR(ppi)->wrMode == NON_WR) || (wr_msg_id == ANN_SUFIX)) {
 		PP_PRINTF("BUG: Trying to send invalid wr_msg mode=%x id=%x",
-			  DSPOR(ppi)->wrMode, wr_msg_id);
+			  WR_DSPOR(ppi)->wrMode, wr_msg_id);
 		return 0;
 	}
 