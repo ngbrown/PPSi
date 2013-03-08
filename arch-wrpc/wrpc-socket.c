ngbrown/PPSi

commit e441b48b2f060ea781c380d1fea2b2e537d1e090

WR: remove used fields in TimeInternal
The three "raw" fields claim to exist for debugging, but since we
have several TimeInternal structures around they cost quite some space.
The fields were only copied to TimeInternal from network frames,
and there is not explicit code to dump them.

Thus, this commit removes them but leaves the code in place, so it can
be re-enabled if the need arises. (I use the ugly "#if 0").

Signed-off-by: Alessandro Rubini <rubini@gnudd.com>

arch-wrpc/wrpc-socket.c
@@ -56,9 +56,11 @@ static int wrpc_net_recv(struct pp_instance *ppi, void *pkt, int len,
 		t->nanoseconds = wr_ts.nsec;
 		t->phase = wr_ts.phase;
 		t->correct = wr_ts.correct;
+#if 0 /* I disabled the fields, for space: they were only used here */
 		t->raw_nsec = wr_ts.raw_nsec;
 		t->raw_ahead = wr_ts.raw_ahead;
 		t->raw_phase = wr_ts.raw_phase;
+#endif
 	}
 	return got;
 }

include/ppsi/ieee1588_types.h
@@ -66,10 +66,16 @@ typedef struct TimeInternal {
 	/* White Rabbit extension begin */
 	Integer32	phase;
 	int 		correct;
-	/* raw time (non-linearized) for debugging purposes */
+#if 0
+	/*
+	 * The following three fields may be used for diagnostics, but
+	 * they cost space. So remove them but keep the code around just
+	 * in case it is useful again (they are only set, never read)
+	 */
 	int32_t raw_phase;
 	int32_t raw_nsec;
 	int32_t raw_ahead;
+#endif
 	/* White Rabbit extension end */
 } TimeInternal;
 

commit 3e2ad33be81028b964cce7f3de1f6054f0121399

bugfix: wr: the raw_ahead field is used by wrpc-sw
the field is used in wrpc-sw, so move it out of "#if 0".
Bug introduced by e441b48b, 134 commits ago.

Signed-off-by: Alessandro Rubini <rubini@gnudd.com>

arch-wrpc/wrpc-socket.c
@@ -54,9 +54,9 @@ static int wrpc_net_recv(struct pp_instance *ppi, void *pkt, int len,
 		t->correct = wr_ts.correct;
 #if 0 /* I disabled the fields, for space: they were only used here */
 		t->raw_nsec = wr_ts.raw_nsec;
-		t->raw_ahead = wr_ts.raw_ahead;
 		t->raw_phase = wr_ts.raw_phase;
 #endif
+		t->raw_ahead = wr_ts.raw_ahead;
 	}
 	return got;
 }

include/ppsi/ieee1588_types.h
@@ -66,14 +66,14 @@ typedef struct TimeInternal {
 	int 		correct;	/* 0 or 1 */
 #if 0
 	/*
-	 * The following three fields may be used for diagnostics, but
+	 * The following two fields may be used for diagnostics, but
 	 * they cost space. So remove them but keep the code around just
 	 * in case it is useful again (they are only set, never read)
 	 */
 	int32_t raw_phase;
 	int32_t raw_nsec;
-	int32_t raw_ahead;
 #endif
+	int32_t raw_ahead;	/* raw_ahead is used during calibration */
 	/* White Rabbit extension end */
 } TimeInternal;
 