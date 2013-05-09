26ade8970c4e80a7758823958dd55d7c0b3087a0
Alessandro Rubini <rubini@gnudd.com>
2013-05-09 14:52:04 +0200

    trivial: add pp_sprintf prototype
    Signed-off-by: Alessandro Rubini <rubini@gnudd.com>

--- include/ppsi/ppsi.h
+++ include/ppsi/ppsi.h
@@ -21,6 +21,8 @@ extern int pp_printf(const char *fmt, ...)
 	__attribute__((format(printf, 1, 2)));
 extern int pp_vprintf(const char *fmt, va_list args)
 	__attribute__((format(printf, 1, 0)));
+extern int pp_sprintf(char *s, const char *fmt, ...)
+	__attribute__((format(printf,2,3)));
 extern int pp_vsprintf(char *buf, const char *, va_list)
 	__attribute__ ((format (printf, 2, 0)));
 
