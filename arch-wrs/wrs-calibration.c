aad33b50f84a805126709704444bf9f69e217bac
Aurelio Colosimo <aurelio@aureliocolosimo.it>
2013-05-24 16:14:45 +0200

    arch-wrs/wrs-calibration.c: new file, containing placeholders for calib functions
    Signed-off-by: Aurelio Colosimo <aurelio@aureliocolosimo.it>

---
+++ arch-wrs/wrs-calibration.c
@@ -0,0 +1,66 @@
+/*
+ * Aurelio Colosimo for CERN, 2012 -- public domain
+ */
+
+#include <ppsi/ppsi.h>
+
+static int wrs_read_calibration_data(struct pp_instance *ppi,
+  uint32_t *deltaTx, uint32_t *deltaRx, int32_t *fix_alpha,
+  int32_t *clock_period)
+{
+	/* FIXME wrs_read_calibration_data */
+	return 0;
+}
+
+int wrs_calibrating_disable(struct pp_instance *ppi, int txrx)
+{
+	return 0;
+}
+
+int wrs_calibrating_enable(struct pp_instance *ppi, int txrx)
+{
+	return 0;
+}
+
+int wrs_calibrating_poll(struct pp_instance *ppi, int txrx, uint32_t *delta)
+{
+	/* FIXME wrpc_calibrating_poll */
+	return 0;
+}
+
+int wrs_calibration_pattern_enable(struct pp_instance *ppi,
+				    unsigned int calibrationPeriod,
+				    unsigned int calibrationPattern,
+				    unsigned int calibrationPatternLen)
+{
+	/* FIXME wrs_calibration_pattern_enable */
+	return 0;
+}
+
+int wrs_calibration_pattern_disable(struct pp_instance *ppi)
+{
+	/* FIXME wrpc_calibration_pattern_disable */
+	return 0;
+}
+
+int wr_calibrating_disable(struct pp_instance *ppi, int txrx)
+	__attribute__((alias("wrs_calibrating_disable")));
+
+int wr_calibrating_enable(struct pp_instance *ppi, int txrx)
+	__attribute__((alias("wrs_calibrating_enable")));
+
+int wr_calibrating_poll(struct pp_instance *ppi, int txrx, uint32_t *delta)
+	__attribute__((alias("wrs_calibrating_poll")));
+
+int wr_calibration_pattern_enable(struct pp_instance *ppi,
+	unsigned int calibrationPeriod, unsigned int calibrationPattern,
+	unsigned int calibrationPatternLen)
+	__attribute__((alias("wrs_calibration_pattern_enable")));
+
+int wr_calibration_pattern_disable(struct pp_instance *ppi)
+	__attribute__((alias("wrs_calibration_pattern_disable")));
+
+int wr_read_calibration_data(struct pp_instance *ppi,
+	uint32_t *deltaTx, uint32_t *deltaRx, int32_t *fix_alpha,
+	int32_t *clock_period)
+	__attribute__((alias("wrs_read_calibration_data")));
