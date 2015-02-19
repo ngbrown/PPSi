4f7246d87895ed100813a83a83bb31738210c066
Adam Wujek <adam.wujek@cern.ch>
2015-02-19 18:30:28 +0100

    arch-wrs: remove get_sync_state IPC call
    This call is not needed since all status info is exported via shmem.
    
    Signed-off-by: Adam Wujek <adam.wujek@cern.ch>

--- arch-wrs/wrs-ipcserver.c
+++ arch-wrs/wrs-ipcserver.c
@@ -10,20 +10,10 @@
 #include <hal_exports.h>
 #include <wr-api.h>
 
-extern ptpdexp_sync_state_t cur_servo_state;
-
 /* minipc Encoding  of the supported commands */
 
 #define PTPDEXP_COMMAND_TRACKING 1
 #define PTPDEXP_COMMAND_MAN_ADJUST_PHASE 2
-
-static struct minipc_pd __rpcdef_get_sync_state = {
-	.name = "get_sync_state",
-	.retval = MINIPC_ARG_ENCODE(MINIPC_ATYPE_STRUCT, ptpdexp_sync_state_t),
-	.args = {
-			MINIPC_ARG_END,
-	},
-};
 
 static struct minipc_pd __rpcdef_cmd = {
 	.name = "cmd",
@@ -35,15 +25,6 @@ static struct minipc_pd __rpcdef_cmd = {
 	},
 };
 
-/* Fill struct ptpdexp_sync_state_t with current servo state */
-static int wrsipc_get_sync_state(ptpdexp_sync_state_t *state)
-{
-	if (!BUILT_WITH_WHITERABBIT)
-		cur_servo_state.valid = 0; /* unneeded, likely */
-	memcpy(state, &cur_servo_state, sizeof(*state));
-	return 0;
-}
-
 /* Execute command coming ipc */
 static int wrsipc_cmd(int cmd, int value)
 {
@@ -54,19 +35,6 @@ static int wrsipc_cmd(int cmd, int value)
 		if(cmd == PTPDEXP_COMMAND_MAN_ADJUST_PHASE)
 			wr_servo_man_adjust_phase(value);
 	}
-	return 0;
-
-}
-
-/* Two functions to manage packet/args conversions */
-static int export_get_sync_state(const struct minipc_pd *pd,
-				 uint32_t *args, void *ret)
-{
-	ptpdexp_sync_state_t state;
-
-	wrsipc_get_sync_state(&state);
-
-	*(ptpdexp_sync_state_t *)ret = state;
 	return 0;
 
 }
@@ -83,9 +51,7 @@ static int export_cmd(const struct minipc_pd *pd,
 /* To be called at startup, right after the creation of server channel */
 void wrs_init_ipcserver(struct minipc_ch *ppsi_ch)
 {
-	__rpcdef_get_sync_state.f = export_get_sync_state;
 	__rpcdef_cmd.f = export_cmd;
 
-	minipc_export(ppsi_ch, &__rpcdef_get_sync_state);
 	minipc_export(ppsi_ch, &__rpcdef_cmd);
 }
