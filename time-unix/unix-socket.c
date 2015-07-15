ngbrown/PPSi

commit d998ea4ba04391e8d169413c7e522bcb83f9cbd4

unix socket handling: fix select timeouts in multilink context
The select must be always called with the least required timeout.
This patch fixes a bug which caused an undesired delay when a state
machine, after receiving a packet, needed to be called soon (e.g.
when it has just changed its status: its desired delay is 0).

Signed-off-by: Aurelio Colosimo <aurelio@aureliocolosimo.it>

time-unix/unix-socket.c
@@ -401,8 +401,13 @@ int unix_net_check_pkt(struct pp_globals *ppg, int delay_ms)
 	int ret = 0;
 	int maxfd = 0;
 	struct unix_arch_data *arch_data = POSIX_ARCH(ppg);
+	int old_delay_ms;
 
-	if (delay_ms != -1) {
+	old_delay_ms = arch_data->tv.tv_sec * 1000 +
+		arch_data->tv.tv_usec / 1000;
+
+	if ((delay_ms != -1) &&
+		((old_delay_ms == 0) || (delay_ms < old_delay_ms))) {
 		/* Wait for a packet or for the timeout */
 		arch_data->tv.tv_sec = delay_ms / 1000;
 		arch_data->tv.tv_usec = (delay_ms % 1000) * 1000;

arch-unix/main-loop.c
@@ -100,6 +100,7 @@ void unix_main_loop(struct pp_globals *ppg)
 		delay_ms = -1;
 
 		for (j = 0; j < ppg->nlinks; j++) {
+			int tmp_d;
 			ppi = &ppg->pp_instances[j];
 
 			if ((NP(ppi)->ch[PP_NP_GEN].pkt_present) ||
@@ -121,8 +122,11 @@ void unix_main_loop(struct pp_globals *ppg)
 					continue;
 				}
 
-				pp_state_machine(ppi, ppi->rx_ptp,
+				tmp_d = pp_state_machine(ppi, ppi->rx_ptp,
 					i - NP(ppi)->ptp_offset);
+
+				if ((delay_ms == -1) || (tmp_d < delay_ms))
+					delay_ms = tmp_d;
 			}
 		}
 	}

arch-wrs/main-loop.c
@@ -103,6 +103,7 @@ void wrs_main_loop(struct pp_globals *ppg)
 		delay_ms = -1;
 
 		for (j = 0; j < ppg->nlinks; j++) {
+			int tmp_d;
 			ppi = &ppg->pp_instances[j];
 
 			if ((NP(ppi)->ch[PP_NP_GEN].pkt_present) ||
@@ -124,8 +125,11 @@ void wrs_main_loop(struct pp_globals *ppg)
 					continue;
 				}
 
-				pp_state_machine(ppi, ppi->rx_ptp,
+				tmp_d = pp_state_machine(ppi, ppi->rx_ptp,
 					i - NP(ppi)->ptp_offset);
+
+				if ((delay_ms == -1) || (tmp_d < delay_ms))
+					delay_ms = tmp_d;
 			}
 		}
 	}

commit 78514d4c236b7b5b21671e9b7363fd2dd8a34fb7

Correct bug where the select timeout decreases to a minimum and stays there. Resets the old_delay_ms to 0 for the next run when select has timed out. Bug introduced in d998ea4ba04391e8d169413c7e522bcb83f9cbd4.

time-unix/unix-socket.c
@@ -580,8 +580,12 @@ static int unix_net_check_packet(struct pp_globals *ppg, int delay_ms)
 	if (i < 0)
 		return -1;
 
-	if (i == 0)
+	if (i == 0) {
+		/* the time limit expired, so zero out the old_delay_ms for the next run */
+		arch_data->tv.tv_sec = 0;
+		arch_data->tv.tv_usec = 0;
 		return 0;
+	}
 
 	for (j = 0; j < ppg->nlinks; j++) {
 		struct pp_instance *ppi = INST(ppg, j);